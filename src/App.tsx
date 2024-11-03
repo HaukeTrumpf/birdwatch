ea11c163480158d57f3d2efd2bace7d0989b4f5b
hauke <hauke_trumpf@hotmail.de>
2024-11-03 12:50:28 +0100

    birdwatch BRE

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import ImageUploader from './components/ImageUploader';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Image Upload</h1>
+      <h1 className='text-xxl font-bold mb-4'>Vogelcam</h1>
       <ImageUploader />
     </div>
   );
